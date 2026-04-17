395655ef44bce37cd4c1d47a0004f0c3d7a99086
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-04-17 10:49:44 +0600

    fixed image path for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from "react-router/dom";
+import { router } from "./route/Routes";
+import TimelineProvider from './components/context/TimelineProvider';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TimelineProvider> 
+          <RouterProvider router={router} />,
+    </TimelineProvider>
+      
   </StrictMode>,
 )
+ 
